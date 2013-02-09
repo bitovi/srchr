f45d3fdc22821ecdb1de4bece18a56998aecb3cb
Farhana Basrawala (fara82) <fbasrawala@gmail.com>
2013-02-09 02:14:52 -0500

    update srchr.history module

--- srchr/history/history.js
+++ srchr/history/history.js
@@ -1,86 +1,71 @@
-steal('jquery/controller',
-	'jquery/model/list/cookie',
-	'jquery/view/ejs',
-	'jquery/controller/view',
-	'steal/less').then('./history.less', function($){
+ steal(
+ 	'can',
+ 	'/srchr/srchr/models/history.js',
+ 	'./init.ejs',
+	//'./history.less', 
+	function(can, History, initEJS){
 
-/**
- * Provides a cookie-stored list of model instances. 
- * It allows you to remove these items from the list. 
- * @tag controllers, home
- */
-$.Controller("Srchr.History",
-/* @static */
-{
-	defaults : {
-		//what cookie to store this in
-		storeName : "searchHistory",
-		//returns html to be displayed for each item on the list
-		titleHelper : function(instance){
-			return "Item"+instance[instance.Class.id];
-		}
-	}
-},
-/* @prototype */
-{
 	/**
-	 * Waits for the page to be loaded
+	 * Provides a list of model instances stored in localStorage. 
+	 * It allows you to remove these items from the list. 
+	 * @tag controllers, home
 	 */
-	init : function(){
-		this.instances = new $.Model.List.Cookie([]).retrieve(this.options.storeName);
-		this.append(this.instances);
-		
-	},
-	/**
-	 * Adds an instance to this list.
-	 * @param {Object} newInstance The data to add to the instances list.
-	 */
-	add : function(newInstance){
-		
-		if(!this.instances.get(newInstance).length){
-			this.instances.push(newInstance);
-			this.append([newInstance]);
+	return can.Control(
+	/* @static */
+	{
+		defaults : {
+			//what localStorage name to store this in
+			storeName : "searchHistory",
+			//returns html to be displayed for each item on the list
+			titleHelper : function(instance){
+				return "HistoryItem"+instance.id;
+			}
 		}
 	},
+	/* @prototype */
+	{
+		/**
+		 * Waits for the page to be loaded
+		 */
+		init : function(){
+			var self = this;
+			History.findAll({}, function(historyList) {
+				self.options.historyList = historyList;
+		    	self.element.html( initEJS(self.options) );
+		    });			
+		},
+		/**
+		 * Adds an instance to this list.
+		 * @param {Object} newInstance The data to add to the instances list.
+		 */
+		"{History} created" : function(list, ev, historyItem){
+			this.options.historyList.push(historyItem);
+		},
 	
-	/**
-	 * Add some history entry instances to the list.
-	 * @param {Object} instances The instances to add.
-	 */
-	append : function(instances){
-		
-		this.element.append(this.view('add',{
-			data: instances,
-			titleHelper : this.options.titleHelper
-		}));
-		this.instances.store(this.options.storeName);
-	},
+		/**
+		 * Binds the "remove" class on click.  Removes a history entry.
+		 * @param {Object} el The history event to remove.
+		 * @param {Object} ev The event that was fired.
+		 */
+		".remove click" : function(el, ev){
+			var li = el.closest('li'),
+				toBeRemoved = li.data('history');
+			
+			li.fadeOut(function(){
+				toBeRemoved.destroy();
+			});
+			ev.stopImmediatePropagation();
+		},
 	
-	/**
-	 * Binds the "remove" class on click.  Removes a history entry.
-	 * @param {Object} el The history event to remove.
-	 * @param {Object} ev The event that was fired.
-	 */
-	".remove click" : function(el, ev){
-		var li = el.closest('li'),
-			toBeRemoved = li.model();
-		this.instances.remove(toBeRemoved);
-		this.instances.store(this.options.storeName);
-		
-		li.fadeOut(function(){
-			li.remove();
-		});
-		ev.stopImmediatePropagation();
-	},
-	
-	/**
-	 * Fires "selected" and passes el.model().
-	 * @param {Object} el The history entry that was clicked
-	 * @param {Object} ev The event that was fired.
-	 */
-	"li click" : function(el, ev){
-		el.trigger("selected", el.model())
-	}
-});
-	
+		/**
+		 * Fires "selected" and passes el.model().
+		 * @param {Object} el The history entry that was clicked
+		 * @param {Object} ev The event that was fired.
+		 */
+		"li click" : function(el, ev){
+			var selected = el.data('history');
+
+			el.trigger("selected", selected);
+		}
+	});
 });
