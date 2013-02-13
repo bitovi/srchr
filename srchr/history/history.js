e61b4d0fb9401ee876ee8b391045e8bbda0e9abe
Farhana Basrawala (fara82) <fbasrawala@gmail.com>
2013-02-13 03:57:17 -0500

    fix syntax

--- srchr/history/history.js
+++ srchr/history/history.js
@@ -1,9 +1,4 @@
- steal(
- 	'can',
- 	'/srchr/srchr/models/history.js',
- 	'./init.ejs',
-	//'./history.less', 
-	function(can, History, initEJS){
+steal('can', '/srchr/srchr/models/history.js', './init.ejs', './history.less', function( can, History, initEJS ) {
 
 	/**
 	 * Provides a list of model instances stored in localStorage. 
@@ -13,10 +8,10 @@
 	return can.Control(
 	/* @static */
 	{
-		defaults : {
+		defaults: {
 			//returns html to be displayed for each item on the list
-			titleHelper : function(instance){
-				return "HistoryItem"+instance.id;
+			titleHelper: function( instance ) {
+				return "HistoryItem" + instance.id;
 			}
 		}
 	},
@@ -25,42 +20,42 @@
 		/**
 		 * Waits for the page to be loaded
 		 */
-		init : function(){
+		init: function() {
 			var self = this;
-			History.findAll({}, function(historyList) {
+			History.findAll({}, function( historyList ) {
 				self.options.historyList = historyList;
-		    	self.element.html( initEJS(self.options) );
-		    });			
+				self.element.html(initEJS(self.options));
+			});
 		},
 		/**
 		 * Adds an instance to this list.
 		 * @param {Object} newInstance The data to add to the instances list.
 		 */
-		"{History} created" : function(list, ev, historyItem){
+		"{History} created": function( list, ev, historyItem ) {
 			this.options.historyList.push(historyItem);
 		},
-	
+
 		/**
 		 * Binds the "remove" class on click.  Removes a history entry.
 		 * @param {Object} el The history event to remove.
 		 * @param {Object} ev The event that was fired.
 		 */
-		".remove click" : function(el, ev){
+		".remove click": function( el, ev ) {
 			var li = el.closest('li'),
 				toBeRemoved = li.data('history');
-			
-			li.fadeOut(function(){
+
+			li.fadeOut(function() {
 				toBeRemoved.destroy();
 			});
 			ev.stopImmediatePropagation();
 		},
-	
+
 		/**
 		 * Fires "selected" and passes el.model().
 		 * @param {Object} el The history entry that was clicked
 		 * @param {Object} ev The event that was fired.
 		 */
-		"li click" : function(el, ev){
+		"li click": function( el, ev ) {
 			var selected = el.data('history');
 
 			el.trigger("selected", selected);
