2e9629a30ff3929b193559540f4e31461d305bb7
Justin Meyer <justinbmeyer@gmail.com>
2013-02-18 16:53:21 -0600

    better formatted test

--- srchr/srchr_test.js
+++ srchr/srchr_test.js
@@ -1,87 +1,86 @@
-steal(
-	'funcunit'/*,
+steal('funcunit',
 	'srchr/history/history_test.js',
 	'srchr/models/models_test.js',
 	'srchr/search/search_test.js',
 	'srchr/search/search_result_test.js',
-	'srchr/tabs/tabs_test.js'*/,function(S){
+	'srchr/tabs/tabs_test.js',
+	function( S ) {
 
 
-module("srchr", { 
-	setup: function(){
-        S.open("//srchr/srchr.html");
-	},
-	checkYahooOnlyResults : function(){
-		ok(S('#flickr').html(), 'Results were retrieved.')
-		ok(!S('#resultsTab li:eq(0)').hasClass('disabled'), "Selected tab Twitter is enabled.")
-		ok(S('#resultsTab li:eq(1)').hasClass('disabled'), "Non-selected tab Upcoming is disabled.")
-		ok(S('#resultsTab li:eq(2)').hasClass('disabled'), "Non-selected tab Flickr is disabled.")
-	}
-});
-
-
-
-test('Search shows results in selected service', function(){
-	
-	S('input[value=Twitter]').click();
-	S('#query').click().type('Dogs\r');
-	
-	// wait until there are 2 results
-	S("#Twitter li").exists( function(){
-		
-		ok(true, "We see results in twitter");
-		// make sure we see dogs in the history
-		
-		var r = /Dogs\s+t/;
-		
-		ok(r.test(S("#history li.selected").text()), "we see dogs correctly");
-		
-		// make sure flickr and everyone else is diabled
-		ok(S('#resultsTab li:contains(Flickr)').hasClass('disabled'), "Flickr is disabled.");
-		ok(S('#resultsTab li:contains(Google)').hasClass('disabled'), "Google is disabled.");
-	}); 
-	
-	
-	
-})
-
-test('Switching results tabs', function(){
-	S('input[value=Twitter]').click();
-	S('input[value=Flickr]').click();
-	
-	S('#query').click().type('Cats\r');
-	
-	S("#Flickr li").size(function(size){
-		return size > 1
-	}, function(){
-	
-		equals(S('#Flickr').css('display'), 'block', 'Twitter results panel is visible')
-		
-	})
-	
-	S('#resultsTab li:contains(Twitter) a').exists().click(function(){
-		equals(S('#Flickr').css('display'), 'none', 'Twitter results panel is hidden')
-		equals(S('#Twitter').css('display'), 'block', 'Flickr results panel is visible')
-	})
-})
-
-test('Clicking history entries re-creates the search', function(){
-	S('#history li:contains(Dogs)').click(function(){
-		equals(S('#query').val(), "Dogs", 'Dogs was put back into the query field')
+	module("srchr", {
+		setup: function() {
+			S.open("//srchr/srchr.html");
+		},
+		checkYahooOnlyResults: function() {
+			ok(S('#flickr').html(), 'Results were retrieved.')
+			ok(!S('#resultsTab li:eq(0)').hasClass('disabled'), "Selected tab Twitter is enabled.")
+			ok(S('#resultsTab li:eq(1)').hasClass('disabled'), "Non-selected tab Upcoming is disabled.")
+			ok(S('#resultsTab li:eq(2)').hasClass('disabled'), "Non-selected tab Flickr is disabled.")
+		}
 	});
-	S("#Twitter li.result").exists( function(){
-		ok(true, "We see results in twitter");
+
+
+
+	test('Search shows results in selected service', function() {
+
+		S('input[value=Twitter]').click();
+		S('#query').click().type('Dogs\r');
+
+		// wait until there are 2 results
+		S("#Twitter li").exists(function() {
+
+			ok(true, "We see results in twitter");
+			// make sure we see dogs in the history
+			var r = /Dogs\s+t/;
+
+			ok(r.test(S("#history li.selected").text()), "we see dogs correctly");
+
+			// make sure flickr and everyone else is diabled
+			ok(S('#resultsTab li:contains(Flickr)').hasClass('disabled'), "Flickr is disabled.");
+			ok(S('#resultsTab li:contains(Google)').hasClass('disabled'), "Google is disabled.");
+		});
+
+
+
 	})
-})
+
+	test('Switching results tabs', function() {
+		S('input[value=Twitter]').click();
+		S('input[value=Flickr]').click();
+
+		S('#query').click().type('Cats\r');
+
+		S("#Flickr li").size(function( size ) {
+			return size > 1
+		}, function() {
+
+			equals(S('#Flickr').css('display'), 'block', 'Twitter results panel is visible')
+
+		})
+
+		S('#resultsTab li:contains(Twitter) a').exists().click(function() {
+			equals(S('#Flickr').css('display'), 'none', 'Twitter results panel is hidden')
+			equals(S('#Twitter').css('display'), 'block', 'Flickr results panel is visible')
+		})
+	})
+
+	test('Clicking history entries re-creates the search', function() {
+		S('#history li:contains(Dogs)').click(function() {
+			equals(S('#query').val(), "Dogs", 'Dogs was put back into the query field')
+		});
+		S("#Twitter li.result").exists(function() {
+			ok(true, "We see results in twitter");
+		})
+	})
 
 
-test('All history entries are deletable', function(){
-	for (var i = S('#history li').size() - 1; i > -1; i--){
-		S('#history li a.remove:eq(' + i + ')').click()
-	}
-	
-	S('#history li').size(0)
-	ok(S('#history li').size(), 'All history entries were removed.')
-})
+	test('All history entries are deletable', function() {
+		for ( var i = S('#history li').size() - 1; i > -1; i-- ) {
+			S('#history li a.remove:eq(' + i + ')').click()
+		}
+
+		S('#history li').size(0)
+		ok(S('#history li').size(), 'All history entries were removed.')
+	})
 
 });
